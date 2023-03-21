cb615e5577dde7ce7dda9d9445c37712f99e6274
Adam Case <adamrichcase@gmail.com>
2023-03-20 20:10:28 -0700

    straight up not going to check for the apt atm

--- pages/response_people.php
+++ pages/response_people.php
@@ -67,9 +67,9 @@ while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
 
     print "<td>";
     print "$row[street_num] $row[street]<br>";
-    if ($row[apt] != "") {
-      print "apt $row[apt]<br>";
-    }
+    // if ($row[apt] != "") {
+    //   print "apt $row[apt]<br>";
+    // }
     print "$row[city], $row[state_code]";
     print "</td>";
     print "</tr>";
