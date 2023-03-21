324c31e51d976808141de6bef0d7d1f8069d73b5
Adam Case <adamrichcase@gmail.com>
2023-03-20 21:10:26 -0700

    correction to is null

--- pages/wo_filter.php
+++ pages/wo_filter.php
@@ -51,7 +51,7 @@ print "</form>";
 <hr>
 
 <?php
-if (isset($_POST['type'])) {
+if (!is_null($_POST['type'])) {
 $query = "SELECT id, summary FROM work_order ";
 $query = $query."WHERE type=$_POST[type];";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
